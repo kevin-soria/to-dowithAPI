971d767ffbd0e7406d8c12c36f1be4de4c4e7ecf
kevin-soria <kevin.soria@outlook.com>
2020-02-27 14:14:59 -0500

    third

--- src/App.js
+++ src/App.js
@@ -6,30 +6,42 @@ function App() {
 
     const [todo,
         setTodo] = useState([]);
-    const [input, setInput] = useState('')
+    const [input,
+        setInput] = useState('')
     console.log(input)
     console.log(todo)
 
     const handleClick = (event) => {
-      return setInput(event.target.value)
+        return setInput(event.target.value)
     }
 
     const handleKeyDown = (event) => {
         if (event.keyCode === 13) {
-          setTodo([...todo, input])
+            setTodo([
+                ...todo,
+                input
+            ]);
+            setInput("")
         }
     }
 
     const list = todo.map((item, index) => <Cards key={index} nameL={item}/>)
 
     return (
-        <div className="container1">
+      <div className="container1">
+
+        <div className="heading">
+          <h1>To-Do List!!!</h1>
+          <h2> Get These Done!!!!</h2>
+        </div>
+
             <div className='input'>
                 <input
                     value={input}
                     type="text"
                     id="input"
                     onChange={(event) => handleClick(event)}
+                    placeholder= "Add a Task :)"
                     onKeyDown={(event) => handleKeyDown(event)}/>
             </div>
             <div className="cardTable">
