2feeaaac7d0a44d733195d39ddd44d478480770e
kevin-soria <kevin.soria@outlook.com>
2020-02-26 21:50:32 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Cards from "./js/components/Cards/cards"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todo,
+        setTodo] = useState([]);
+    const [input, setInput] = useState('')
+    console.log(input)
+    console.log(todo)
+
+    const handleClick = (event) => {
+      return setInput(event.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 13) {
+          setTodo([...todo, input])
+        }
+    }
+
+    const list = todo.map((item, index) => <Cards key={index} nameL={item}/>)
+
+    return (
+        <div className="container1">
+            <div className='input'>
+                <input
+                    value={input}
+                    type="text"
+                    id="input"
+                    onChange={(event) => handleClick(event)}
+                    onKeyDown={(event) => handleKeyDown(event)}/>
+            </div>
+            <div className="cardTable">
+                {list}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/js/components/Cards/cards.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./cards.css";
+
+
+const Cards = (props) => {
+
+
+return(
+    <div className="newTask">
+        <h1>{props.nameL}</h1>
+    </div>
+)
+
+}
+
+
+
+export default Cards;
